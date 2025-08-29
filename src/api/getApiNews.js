6ef5f9d9f1c13bb7ba1374dffc56f312582afba1
Crouch <crouch365md@gmail.com>
2025-08-29 14:36:10 +0500

    add latest news

--- src/api/getApiNews.js
+++ src/api/getApiNews.js
@@ -26,6 +26,20 @@ export const getApiNews = async ({
   }
 };
 
+export const getLatestNews = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}latest-news`, {
+      params: {
+        apiKey: API_KEY,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log("Error from func.getCategories", error);
+  }
+};
+
 export const getCategories = async () => {
   try {
     const response = await axios.get(`${BASE_URL}available/categories`, {
